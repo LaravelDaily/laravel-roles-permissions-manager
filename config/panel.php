8497e4278f48f9e24263d9197aac9ba6654280c1
Marius-K <marre124@gmail.com>
2019-10-29 15:04:03 +0200

    Added missing config/panel.php file - Issue #26

---
+++ config/panel.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'date_format'         => 'Y-m-d',
+    'time_format'         => 'H:i:s',
+    'primary_language'    => 'en',
+    'available_languages' => [
+        'en' => 'English',
+    ],
+];
