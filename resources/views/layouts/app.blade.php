7a5f552b4071a05994d97783a00f6e361311f8e3
PovilasKorop <povilask007@gmail.com>
2020-02-21 07:57:13 +0200

    Change CoreUI CDN version to v2.1.16 - fixes #35

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
     <title>{{ env('APP_NAME', 'Permissions Manager') }}</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="https://unpkg.com/@coreui/coreui/dist/css/coreui.min.css" rel="stylesheet" />
+    <link href="https://unpkg.com/@coreui/coreui@2.1.16/dist/css/coreui.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" />
     <link href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" rel="stylesheet" />
     <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet" />
